1ac335ad1f310f651832f09b3d3cbb4bebbe66cb
Shazib2266 <VIRGOMUGHAL2266@GMAIL.COM>
2024-12-03 12:27:14 +0500

    Update config.js

--- config.js
+++ config.js
@@ -5,12 +5,12 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "", //paste your session_id
-AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true", 
+SESSION_ID: process.env.SESSION_ID || "3iglUb4R#9Kte43eU1jSfG0OM9RBmb-HPJsSqgah25HFPBL1BVMw", //paste your session_id
+AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "false", 
 MODE: process.env.MODE || "public",
-AUTO_VOICE: process.env.AUTO_VOICE || "true",
+AUTO_VOICE: process.env.AUTO_VOICE || "false",
 AUTO_STICKER: process.env.AUTO_STICKER || "false",
 AUTO_REPLY: process.env.AUTO_REPLY || "false",
 AUTO_REACT: process.env.AUTO_REACT || "true",
-FAKE_RECORDING: process.env.FAKE_RECORDING || "true",    
+FAKE_RECORDING: process.env.FAKE_RECORDING || "false",    
 };
